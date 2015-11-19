e870b7cb2813e18b8d0343e36d4310b5ea75558a
Luiz Guilherme Silva <luizguilhermeprog@gmail.com>
2015-11-18 22:56:28 -0200

    Mudanca na chamada padrao para que o carquivo possa ser importado

--- numero_da_sorte.py
+++ numero_da_sorte.py
@@ -2,5 +2,8 @@ import random
 def numero_da_sorte():
     l = list(range(1, 61))
     random.shuffle(l)
-    print("-".join([str(i) for i in l[:6]]))
-numero_da_sorte()
+    return "-".join([str(i) for i in l[:6]])
+
+if __name__ == '__main__':
+    print(numero_da_sorte())
+
