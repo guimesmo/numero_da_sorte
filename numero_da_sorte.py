ae5c9ab9b2b64f93a0198b4b0e34c52d3279c621
Luiz Silva <luiz.silva@dextra-sw.com>
2018-12-22 11:58:52 -0200

    Melhor formatação e ordenação dos números

--- numero_da_sorte.py
+++ numero_da_sorte.py
@@ -2,7 +2,9 @@ import random
 def numero_da_sorte():
     l = list(range(1, 61))
     random.shuffle(l)
-    return "-".join([str(i) for i in l[:6]])
+    l = l[:6]
+    l.sort()
+    return " - ".join(["%02d" % i for i in l])
 
 if __name__ == '__main__':
     print(numero_da_sorte())
